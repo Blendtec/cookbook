caee8367dd5dda36ee69fe652371217dadce767a
Stone Lasley <slasley@blendtec.com>
2013-11-01 14:44:00 -0600

    run cake as different user

--- residential/recipes/appsetup.rb
+++ residential/recipes/appsetup.rb
@@ -1,13 +1,13 @@
 node[:deploy].each do |app_name, deploy|
 
-  log "message" do
+  log 'message' do
     message "APPSETUP: installing composer"
     level :info
   end
 
   script "install_composer" do
     interpreter "bash"
-    user "root"
+    user 'root'
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
@@ -15,20 +15,20 @@ node[:deploy].each do |app_name, deploy|
     EOH
   end
 
-  log "message" do
+  log 'message' do
     message "APPSETUP: generating database.php"
     level :info
   end
 
   template "#{deploy[:deploy_to]}/current/app/Config/database.php" do
-    source "database.php.erb"
+    source 'database.php.erb'
     mode 0660
     group deploy[:group]
 
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")
-      owner "apache"
+    if platform?('ubuntu')
+      owner 'www-data'
+    elsif platform?('amazon')
+      owner 'apache'
     end
 
     variables(
@@ -43,20 +43,20 @@ node[:deploy].each do |app_name, deploy|
     end
   end
 
-  log "message" do
-    message "APPSETUP: generating core.php"
+  log 'message' do
+    message 'APPSETUP: generating core.php'
     level :info
   end
 
   template "#{deploy[:deploy_to]}/current/app/Config/core.php" do
-    source "core.php.erb"
+    source 'core.php.erb'
     mode 0660
     group deploy[:group]
 
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")
-      owner "apache"
+    if platform?('ubuntu')
+      owner 'www-data'
+    elsif platform?('amazon')
+      owner 'apache'
     end
 
     only_if do
@@ -65,55 +65,55 @@ node[:deploy].each do |app_name, deploy|
   end
 
 
-  log "message" do
-    message "APPSETUP: changing permissions to cake"
+  log 'message' do
+    message 'APPSETUP: changing permissions to cake'
     level :info
   end
 
   file "#{deploy[:deploy_to]}/current/lib/Cake/Console/cake" do
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")
-      owner "apache"
+    if platform?('ubuntu')
+      owner 'www-data'
+    elsif platform?('amazon')
+      owner 'apache'
     end
     group deploy[:group]
     mode 0755
     action :touch
   end
 
-  log "message" do
-    message "APPSETUP: creating tmp directory"
+  log 'message' do
+    message 'APPSETUP: creating tmp directory'
     level :info
   end
 
   directory "#{deploy[:deploy_to]}/current/app/tmp" do
     mode 0777
     group deploy[:group]
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")
-      owner "apache"
+    if platform?('ubuntu')
+      owner 'www-data'
+    elsif platform?('amazon')
+      owner 'apache'
     end
     action :create
   end
 
-  log "message" do
-    message "APPSETUP: creating tmp/cache directory"
+  log 'message' do
+    message 'APPSETUP: creating tmp/cache directory'
     level :info
   end
 
   directory "#{deploy[:deploy_to]}/current/app/tmp/cache" do
     mode 0777
     group deploy[:group]
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")
-      owner "apache"
+    if platform?('ubuntu')
+      owner 'www-data'
+    elsif platform?('amazon')
+      owner 'apache'
     end
     action :create
   end
 
-  log "message" do
+  log 'message' do
     message "APPSETUP: creating tmp/cache/{models, persistent, views} directories"
     level :info
   end
@@ -122,31 +122,36 @@ node[:deploy].each do |app_name, deploy|
     directory "#{deploy[:deploy_to]}/current/app/tmp/cache/#{dir}" do
       mode 00777
       group deploy[:group]
-      if platform?("ubuntu")
-        owner "www-data"
-      elsif platform?("amazon")
-        owner "apache"
+      if platform?('ubuntu')
+        owner 'www-data'
+      elsif platform?('amazon')
+        owner 'apache'
       end
       action :create
       recursive true
     end
   end
 
-  log "message" do
+  log 'message' do
     message "APPSETUP: running migrations"
     level :info
   end
 
   Dir.foreach("#{deploy[:deploy_to]}/current/app/Plugin") do |item|
     next if item == '.' or item == '..'
-    log "message" do
+    log 'message' do
       message "APPSETUP: running migrations for #{item}"
       level :info
     end
-    execute "cake migration" do
+    execute 'cake migration' do
       cwd "#{deploy[:deploy_to]}/current/app"
-      command "../lib/Cake/Console/cake"
+      command '../lib/Cake/Console/cake'
       returns 1 #temporary
+      if platform?('ubuntu')
+        user 'www-data'
+      elsif platform?('amazon')
+        user 'apache'
+      end
       action :run
     end
   end

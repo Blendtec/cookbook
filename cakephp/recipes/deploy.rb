0351e66ea3d6d31d532829bfafd5acf4304687b7
Stone Lasley <slasley@blendtec.com>
2013-11-04 08:16:55 -0700

    comments, fixed check so it doesn't fail on machine configuration

--- cakephp/recipes/deploy.rb
+++ cakephp/recipes/deploy.rb
@@ -15,6 +15,7 @@ node[:deploy].each do |app_name, deploy|
     EOH
   end
 
+  #generate database config file
   template "#{deploy[:deploy_to]}/current/app/Config/database.php" do
     source 'database.php.erb'
     mode 0440
@@ -38,6 +39,7 @@ node[:deploy].each do |app_name, deploy|
     end
   end
 
+  #generate core config file
   template "#{deploy[:deploy_to]}/current/app/Config/core.php" do
     source 'core.php.erb'
     mode 0440
@@ -54,6 +56,7 @@ node[:deploy].each do |app_name, deploy|
     end
   end
 
+  #set permissions on cake console
   file "#{deploy[:deploy_to]}/current/lib/Cake/Console/cake" do
     if platform?('ubuntu')
       owner 'www-data'
@@ -65,6 +68,7 @@ node[:deploy].each do |app_name, deploy|
     action :touch
   end
 
+  #set tmp permissions, create if needed
   directory "#{deploy[:deploy_to]}/current/app/tmp" do
     mode 0740
     group deploy[:group]
@@ -76,6 +80,7 @@ node[:deploy].each do |app_name, deploy|
     action :create
   end
 
+  #set cache permissions, create if needed
   directory "#{deploy[:deploy_to]}/current/app/tmp/cache" do
     mode 0740
     group deploy[:group]
@@ -87,6 +92,7 @@ node[:deploy].each do |app_name, deploy|
     action :create
   end
 
+  #create cache subdirectories
   %w{models persistent views}.each do |dir|
     directory "#{deploy[:deploy_to]}/current/app/tmp/cache/#{dir}" do
       mode 0740
@@ -101,21 +107,25 @@ node[:deploy].each do |app_name, deploy|
     end
   end
 
-  Dir.foreach("#{deploy[:deploy_to]}/current/app/Plugin") do |item|
-    next if item == '.' or item == '..'  or Dir["#{deploy[:deploy_to]}/current/app/Plugin/#{item}/Config/Migration"].empty?
-    execute 'cake migration' do
-      cwd "#{deploy[:deploy_to]}/current/app"
-      command "../lib/Cake/Console/cake Migrations.migration run all --plugin #{item}"
-      if platform?('ubuntu')
-        user 'www-data'
-      elsif platform?('amazon')
-        user 'apache'
+  #if plugins directory exists iterate over each doing migrations for those with migration scripts
+  if File.directory?("#{deploy[:deploy_to]}/current/app/Plugin")
+    Dir.foreach("#{deploy[:deploy_to]}/current/app/Plugin") do |item|
+      next if item == '.' or item == '..'  or Dir["#{deploy[:deploy_to]}/current/app/Plugin/#{item}/Config/Migration"].empty?
+      execute 'cake migration' do
+        cwd "#{deploy[:deploy_to]}/current/app"
+        command "../lib/Cake/Console/cake Migrations.migration run all --plugin #{item}"
+        if platform?('ubuntu')
+          user 'www-data'
+        elsif platform?('amazon')
+          user 'apache'
+        end
+        action :run
+        returns 0
       end
-      action :run
-      returns 0
     end
   end
 
+  #if app has migrations run them
   if File.directory?("#{deploy[:deploy_to]}/current/app/Config/migration")
     execute 'cake migration' do
       cwd "#{deploy[:deploy_to]}/current/app"

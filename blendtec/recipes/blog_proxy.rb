9a8b4eb226c312a30eca5c3225040fbd1272d76c
Stone Lasley <slasley@blendtec.com>
2014-01-02 23:09:05 -0700

    app name

--- blendtec/recipes/blog_proxy.rb
+++ blendtec/recipes/blog_proxy.rb
@@ -1,18 +1,16 @@
 node[:deploy].each do |app_name, deploy|
 
-  application_name = params[:name]
-
   #Add proxypass entry for blog
   ruby_block "add proxypass blog entry" do
-    Chef::Log.info("modifying #{node[:apache][:dir]}/sites-available/#{application_name}.conf")
+    Chef::Log.info("modifying #{node[:apache][:dir]}/sites-available/#{app_name}.conf")
     block do
-      rc = Chef::Util::FileEdit.new("#{node[:apache][:dir]}/sites-available/#{application_name}.conf")
+      rc = Chef::Util::FileEdit.new("#{node[:apache][:dir]}/sites-available/#{app_name}.conf")
       proxy_line = "YAY"
       rc.insert_line_after_match(/^.*\DocumentRoot\b.*$/, proxy_line)
       rc.write_file
     end
     only_if do
-      ::File.exists?("#{node[:apache][:dir]}/sites-available/#{application_name}.conf")
+      ::File.exists?("#{node[:apache][:dir]}/sites-available/#{app_name}.conf")
     end
   end
 end

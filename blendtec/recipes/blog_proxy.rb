ea6374b515cfe3d00cc02d95382512ffe1c0fa34
Stone Lasley <slasley@blendtec.com>
2014-01-02 21:41:15 -0700

    first file edit try

---
+++ blendtec/recipes/blog_proxy.rb
@@ -0,0 +1,17 @@
+node[:deploy].each do |app_name, deploy|
+
+  application_name = params[:name]
+
+  #Add proxypass entry for blog
+  ruby_block "add proxypass blog entry" do
+    block do
+      rc = Chef::Util::FileEdit.new("#{node[:apache][:dir]}/sites-available/#{application_name}.conf")
+      proxy_line = "YAY"
+      rc.insert_line_after_match ^.*\DocumentRoot\b.*$, proxy_line
+      rc.write_file
+    end
+    only_if do
+      ::File.exists?("#{node[:apache][:dir]}/sites-available/#{application_name}.conf")
+    end
+  end
+end

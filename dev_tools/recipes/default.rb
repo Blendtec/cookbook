d7e8787393805c4ff154294b9376852eb66497b0
Stone Lasley <slasley@blendtec.com>
2014-01-22 16:04:04 -0700

    arcanist recipe

--- dev_tools/recipes/default.rb
+++ dev_tools/recipes/default.rb
@@ -1,43 +1,69 @@
 #checkout Arcanist
-git "/opt/arcanist" do
+git "/home/vagrant/arcanist" do
+  user 'vagrant'
+  group 'vagrant'
   repository 'https://github.com/facebook/arcanist.git'
   reference "master"
   action :sync
 end
 
 #checkout LibPHUtil
-git "/opt/libphutil" do
+git "/home/vagrant/libphutil" do
+  user 'vagrant'
+  group 'vagrant'
   repository 'https://github.com/facebook/libphutil.git'
   reference "master"
   action :sync
 end
 
 #Copy CakePhpTestEngine into Arc
-template "/opt/dev_tools/src/unit/engine/CakePhpTestEngine.php" do
+template "/home/vagrant/arcanist/src/unit/engine/CakePhpTestEngine.php" do
   source 'CakePhpTestEngine.php.erb'
   group 'vagrant'
   owner 'vagrant'
   only_if do
-    File.directory?("/opt/dev_tools")
+    File.directory?("/home/vagrant/arcanist")
   end
 end
 
+log "running /home/vagrant/libphutil/scripts/build_xhpast.sh" do
+  level :debug
+end
+
+#Build xh_past
+script 'build_xhpast' do
+  interpreter 'bash'
+  cwd '/home/vagrant/libphutil/scripts/'
+  code <<-EOH
+    ./build_xhpast.sh
+  EOH
+  only_if do File.exists?("/home/vagrant/libphutil/scripts/build_xhpast.sh") end
+end
+
+#Index arc classes
+script 'arc_liberate' do
+  interpreter 'bash'
+  cwd '/home/vagrant/arcanist'
+  code <<-EOH
+    ./bin/arc liberate
+  EOH
+end
 
 #Add arcanist to path
 script 'arc_path' do
   interpreter 'bash'
   user 'vagrant'
-  cwd '/home/vagrant'
+  cwd '/home/vagrant/'
   code <<-EOH
-    echo "PATH=\"$PATH:/opt/arcanist/bin/\"" >> .profile
+    echo "PATH=\"$PATH:/home/vagrant/arcanist/bin/\"" >> .profile
   EOH
 end
 
-#Install phpunit
+#Install PHPUnit
 script 'phpunit' do
   interpreter 'bash'
   user 'root'
-  cwd '/home/vagrant'
+  cwd '/home/vagrant/'
   code <<-EOH
     pear channel-discover pear.phpunit.de
     pear install phpunit/PHPUnit
@@ -48,7 +74,7 @@ end
 script 'cakephp_codesniffer' do
   interpreter 'bash'
   user 'root'
-  cwd '/home/vagrant'
+  cwd '/home/vagrant/'
   code <<-EOH
     pear channel-discover pear.cakephp.org
     pear install cakephp/CakePHP_CodeSniffer

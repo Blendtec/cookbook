556e1f99a26cf1838be6d6bab395b469ba2901f7
Stone Lasley <slasley@blendtec.com>
2014-01-23 16:59:31 -0700

    moved project checkout into recipe

--- dev_tools/recipes/default.rb
+++ dev_tools/recipes/default.rb
@@ -3,6 +3,17 @@ include_recipe 'dev_tools::arcanist'
 include_recipe 'dev_tools::pear_phpcs'
 include_recipe 'dev_tools::pearphpunit'
 
+#checkout project
+git "/srv/www" do
+  user 'vagrant'
+  group 'www-data'
+  repository 'git@github.com:Blendtec/residential.git'
+  reference "master"
+  enable_submodules true
+  revision develop
+  action :sync
+end
+
 #Copy CakePhpTestEngine into Arc
 template "/home/vagrant/arcanist/src/unit/engine/CakePhpTestEngine.php" do
   source 'CakePhpTestEngine.php.erb'

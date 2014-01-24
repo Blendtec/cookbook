2ef7caacbc7f74d1ecf408c6df8df3de452357de
Stone Lasley <slasley@blendtec.com>
2014-01-24 09:20:43 -0700

    removed cloning from recipe

--- dev_tools/recipes/default.rb
+++ dev_tools/recipes/default.rb
@@ -3,17 +3,6 @@ include_recipe 'dev_tools::arcanist'
 include_recipe 'dev_tools::pear_phpcs'
 include_recipe 'dev_tools::pear_phpunit'
 
-#checkout project
-git "/srv/www" do
-  user 'vagrant'
-  group 'www-data'
-  repository 'git@github.com:Blendtec/residential.git'
-  reference "master"
-  enable_submodules true
-  revision 'develop'
-  action :sync
-end
-
 #Copy CakePhpTestEngine into Arc
 template "/home/vagrant/arcanist/src/unit/engine/CakePhpTestEngine.php" do
   source 'CakePhpTestEngine.php.erb'
